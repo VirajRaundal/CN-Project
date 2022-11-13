e9ee2410c1fa56fee0a66077f75990b66ae6a3da
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2022-11-13 18:03:18 +0530

    initial commit4

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const io = new Server(server);
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
 
 const userSocketMap = {};
